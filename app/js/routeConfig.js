153665a49af845ed34a5af81595deab867b7a3e3
vitortkr <vitortkr@gmail.com>
2018-04-27 17:33:08 -0300

    2704 2

--- app/js/routeConfig.js
+++ app/js/routeConfig.js
@@ -19,19 +19,19 @@ router = new Navigo(null, true, '#!');
 router.on({
   // 'view' is the id of the div element inside which we render the HTML
   'home': function home() {
-   loadHTML('../home.html', 'view');
-   loadHTML('../design.html', 'view');
+   loadHTML('https://vitortkr.github.io/11art/dist/home.html', 'view');
+   loadHTML('https://vitortkr.github.io/11art/dist/design.html', 'view');
   }
 });
 
 // set the default route
 router.on(function () {
-  $id('view').innerHTML = '<h5>Here defou</h5>';
+  $id('view').innerHTML = '<h5></h5>';
 });
 
 // set the 404 route
 router.notFound(function (query) {
-  $id('view').innerHTML = '<h1>NotFound</h1>';
+  $id('view').innerHTML = '<h1></h1>';
 });
 
 router.resolve();
