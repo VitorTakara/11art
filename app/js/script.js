e2de4234a3a6c74d5f1690474b863f2c1cfb5097
vitortkr <vitortkr@gmail.com>
2018-05-03 15:59:17 -0300

    Consertado classe de animacao

--- app/js/script.js
+++ app/js/script.js
@@ -214,10 +214,10 @@
    function initComponents(){
       window.sr = ScrollReveal();
 
-      if(document.querySelectorAll('.animated-default').length > 0)
-         sr.reveal('.animated-default', {duration: 1500});
+      if(document.querySelectorAll('.animation-default').length > 0)
+         sr.reveal('.animation-default', {duration: 1500});
       if(document.querySelectorAll('.animation-comoFunciona').length > 0)
-         sr.reveal('. animation-comoFunciona', {duration: 1000}, 500);
+         sr.reveal('.animation-comoFunciona', {duration: 1000}, 500);
       if(document.querySelectorAll('.animation-modulosServicos').length > 0)
          sr.reveal('.animation-modulosServicos', {duration: 1000}, 500);
       
